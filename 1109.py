445a13aa715899d9f5c3e8e6ef827dbb26fb8993
lizhibo141242 <zeqihappy@163.com>
2021-11-11 06:27:51 +0800

    学习自动化第二次提交

--- 1109.py
+++ 1109.py
@@ -1,6 +1,18 @@
 import os.path
 import pandas as pd 
-df=pd.read_excel(r'D:\108python\莉莉.xls') 
-df
-print('外交部=',df.iloc[4,12])
-os.getcwd()
+df=pd.read_excel(r'D:\108python\sample1.xlsx',header=None) 
+total=df.loc[df[0]=='总计']
+
+row={}
+row['员工ID']=df.iloc[3,7]
+row['姓名']=df.iloc[3,1]
+row['开始时间']=df.iloc[0,7]
+row['结束时间']=df.iloc[0,9]
+row['酒店费用']=total.iloc[0,3]
+row['交通费']=total.iloc[0,4]
+row['油费']=total.iloc[0,5]
+row['餐饮']=total.iloc[0,6]
+row['电话']=total.iloc[0,7]
+row['招待费']=total.iloc[0,8]
+row['杂项']=total.iloc[0,9]
+row['总计']=total.iloc[0,10]
